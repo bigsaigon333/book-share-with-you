3bbd7e27de334b29f47a0c6c6d813eceaa068ab6
bigsaigon333 <likepeppermint@gmail.com>
2020-09-16 00:29:29 +0900

    make it better

--- index.js
+++
@@ -1,9 +0,0 @@
-import app from "./app";
-import dotenv from "dotenv";
-dotenv.config();
-
-const PORT = process.env.PORT;
-
-app.listen(PORT, () => {
-	console.log(`App listening at PORT:${PORT}`);
-});
---
+++ server.js
@@ -0,0 +1,62 @@
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
+}
+
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+const mongoose = require("mongoose");
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// connect to DB
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (error) => console.error(error));
+db.once("open", () =>
+	console.log(`Connedcted to Mongoose: ${process.env.DATABASE_URL}`)
+);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+	next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`App listening at PORT:${PORT}`);
+});
