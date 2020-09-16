3a82b8e06511232017423566926561e3b2f15dee
bigsaigon333 <likepeppermint@gmail.com>
2020-09-17 00:53:20 +0900

    네이버 책 API로 검색기능 추가

--- routes/books.js
+++ routes/books.js
@@ -1,37 +1,62 @@
 const express = require("express");
 const router = express.Router();
-const request = require("request");
-
-// const URL = "https://openapi.naver.com/v1/search/book.json";
-// const option = {
-// 	query: "동",
-// 	start: 1,
-// 	display: 3,
-// };
-
-// request.get(
-//   {
-//     uri: URL,
-//     qs: option,
-//     headers: {
-//       "X-Naver-Client-ID": process.env.NAVER_CLIENT_ID,
-//       "X-Naver-Client-Secret": process.env.NAVER_CLIENT_SECRET,
-//     },
-//   },
-//   (err, res, body) => {
-//     const json = JSON.parse(body);
-//     console.log(json);
-//   }
-// );
 
 router.get("/", (req, res) => {
 	const name = req.body.name;
 
-	res.render("index");
+	res.render("books/index", { book: { name: "Hello World" } });
 });
 
-router.get("/new", (req, res) => {
-	res.render("index");
+router.get("/search", async (req, res) => {
+	const { title } = req.query;
+
+	if (title === undefined) {
+		res.render("books/search", { title: "", books: null });
+	}
+	try {
+		const json = await getBookData(title);
+
+		console.log(json.items);
+		res.render("books/search", { title: title || "", books: json.items });
+	} catch (error) {
+		console.error(error);
+	}
 });
+
+async function getBookData(title) {
+	if (title === undefined || title === null || title === "") return;
+
+	const request = require("request");
+	const fetch = require("node-fetch");
+	const url = new URL("https://openapi.naver.com/v1/search/book.json");
+
+	const options = {
+		query: title,
+		start: 1,
+		display: 3,
+	};
+
+	url.search = new URLSearchParams(options).toString();
+
+	const headers = {
+		"X-Naver-Client-ID": process.env.NAVER_CLIENT_ID,
+		"X-Naver-Client-Secret": process.env.NAVER_CLIENT_SECRET,
+	};
+
+	// console.log(url);
+
+	try {
+		const response = await fetch(url, {
+			method: "GET",
+			// body: options,
+			headers,
+		});
+		const json = await response.json();
+		// console.log(json);
+		return json;
+	} catch (error) {
+		console.error(error);
+	}
+}
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,6 @@ require("dotenv").config();
 var express = require("express");
 var router = express.Router();
 
-const request = require("request");
-
 /* GET home page. */
 router.get("/", (req, res, next) => {
 	console.log("/ here");
